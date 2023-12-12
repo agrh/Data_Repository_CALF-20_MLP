8139c3d0e984ad0e47f5ca830a04cf50e7541be0
agrh <fandong@outlook.com>
2023-12-12 20:45:51 +0100

    Add files via upload

---
+++ phonolammps/README.m
@@ -0,0 +1,6 @@
+Run this example as follows to generate the force constants and show band structure:
+phonolammps  md.in  -c POSCAR_unit_cell --dim 3 3 3  -p
+
+#Note:
+Force constant file larger than 25Mb is not allowed to be posted, please contact the author if required.
+updated [12 Dec. 2023]
